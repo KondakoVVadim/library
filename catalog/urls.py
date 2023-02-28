a7f110f5ed35799d416f2c7a18a6e9236e9b64bc
konda <kondakovvadim13@gmail.com>
2023-02-28 16:54:14 +0200

    1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book/', views.BookListView.as_view(), name='books'),
+    path('book/<int:id>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """
@@ -22,3 +22,16 @@ def index(request):
         'catalog/index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.ListView):
+    pass
