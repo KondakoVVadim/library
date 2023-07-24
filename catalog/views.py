400d232031e9e30045cf3cdcb977796cee7018bf
Огурчик <kondakovvadim13@gmail.com>
2023-07-24 21:31:34 +0300

    Последние штрихи

--- catalog/views.py
+++ catalog/views.py
@@ -286,3 +286,6 @@ class SignUp(CreateView):
             # Если есть ошибки, передаем их в контекст представления
             context = self.get_context_data(form=form)
             return self.render_to_response(context)
+
+class vladpidor:
+    x = 1
